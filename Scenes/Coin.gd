65bf08839dde4efa30bc96dca6cd08ed43b6ac7b
Tralexium <alextheloler@gmail.com>
2023-02-08 23:38:33 +0000

    Added coin spawner

--- Scenes/Coin.gd
+++ Scenes/Coin.gd
@@ -2,23 +2,29 @@ extends Spatial
 
 const COIN_SFX := preload("res://Audio/SFX/coin.wav")
 
-export var rotate_speed := 70.0
+export var velocity := Vector3.ZERO
+export var lifetime := -1
 
+var rotate_speed := 70.0
 var collected := false
 onready var gem: MeshInstance = $Position3D/Gem
 onready var ring: MeshInstance = $Position3D/Ring
 onready var position_3d: Position3D = $Position3D
 onready var chime_loop: AudioStreamPlayer3D = $ChimeLoop
 onready var animation_player: AnimationPlayer = $AnimationPlayer
+onready var lifetime_timer: Timer = $Lifetime
 
 
 func _ready() -> void:
+	if lifetime != -1:
+		lifetime_timer.start(lifetime)
 	position_3d.scale = Vector3.ZERO
 	var tween := create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
 	tween.tween_property(position_3d, "scale", Vector3.ONE, 0.5)
 
 
 func _process(delta: float) -> void:
+	translation += velocity * delta
 	if !collected:
 		gem.rotation_degrees.y += rotate_speed * delta
 		ring.rotation_degrees.y -= rotate_speed * delta
@@ -40,3 +46,7 @@ func _on_Hitbox_body_entered(body: Node) -> void:
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	queue_free()
+
+
+func _on_Lifetime_timeout() -> void:
+	shrink()
---
+++ Scenes/CoinSpawner.gd
@@ -0,0 +1,30 @@
+extends Spatial
+
+const COIN := preload("res://Scenes/Coin.tscn")
+
+
+func spawn_coin(position: Vector3, velocity: Vector3, lifetime: float = -1.0) -> void:
+	var coin_inst := COIN.instance()
+	coin_inst.velocity = velocity
+	coin_inst.lifetime = lifetime
+	coin_inst.translation = position
+	add_child(coin_inst)
+
+
+func spawn_coin_random_spot(positions: Array, velocity: Vector3, lifetime: float = -1.0) -> void:
+	var coin_inst := COIN.instance()
+	coin_inst.velocity = velocity
+	coin_inst.lifetime = lifetime
+	coin_inst.translation = Util.choose(positions)
+	add_child(coin_inst)
+
+
+func spawn_coin_random_offset(random_offset: Vector3, position: Vector3, velocity: Vector3, lifetime: float = -1.0) -> void:
+	var coin_inst := COIN.instance()
+	coin_inst.velocity = velocity
+	coin_inst.lifetime = lifetime
+	coin_inst.translation = position
+	coin_inst.translation += Vector3(randf() * random_offset.x - random_offset.x/2, \
+									 randf() * random_offset.y - random_offset.y/2, \
+									 randf() * random_offset.z - random_offset.z/2)
+	add_child(coin_inst)
